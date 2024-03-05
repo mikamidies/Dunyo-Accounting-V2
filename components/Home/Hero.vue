3b982f6f9eb8d1a4eeac25f4b79592cab63d09eb
mikamidies <mikamidies04@gmail.com>
2024-03-05 12:19:07 +0500

    update

--- components/Home/Hero.vue
+++ components/Home/Hero.vue
@@ -2,7 +2,7 @@
   <div class="wrap">
     <ApplicationModal ref="appModal" />
 
-    <button class="heroPrev">
+    <!-- <button class="heroPrev">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="14"
@@ -33,13 +33,13 @@
           fill="white"
         />
       </svg>
-    </button>
+    </button> -->
 
     <video autoplay muted playsinline loop src="/video.mp4"></video>
     <img src="@/assets/img/hero-bird.png" alt="" class="bird" />
     <div class="container">
       <div class="slider">
-        <div class="swiper heroSwiper">
+        <div class="swiper">
           <div class="swiper-wrapper">
             <div class="swiper-slide">
               <div class="content">
@@ -54,72 +54,6 @@
                 </p>
                 <div class="numbers">
                   <button @click="openAppModal()" class="button">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="18"
-                      height="19"
-                      viewBox="0 0 18 19"
-                      fill="none"
-                    >
-                      <path
-                        d="M18 16.5V14.8541C18 14.0363 17.5021 13.3008 16.7428 12.9971L14.7086 12.1835C13.7429 11.7971 12.6422 12.2156 12.177 13.146L12 13.5C12 13.5 9.5 13 7.5 11C5.5 9 5 6.5 5 6.5L5.35402 6.32299C6.28438 5.85781 6.70285 4.75714 6.31654 3.79136L5.50289 1.75722C5.19916 0.997903 4.46374 0.5 3.64593 0.5H2C0.895431 0.5 0 1.39543 0 2.5C0 11.3366 7.16344 18.5 16 18.5C17.1046 18.5 18 17.6046 18 16.5Z"
-                        fill="#866719"
-                      />
-                    </svg>
-                    {{ $store.state.translations["main.contact-us"] }}
-                  </button>
-                  <a :href="`tel:${info.nbm}`" class="tel">
-                    {{ info?.nbm }}
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div class="swiper-slide">
-              <div class="content">
-                <p class="sup">
-                  {{ $store.state.translations["main.hero-sup"] }}
-                </p>
-                <h4 class="title">
-                  {{ $store.state.translations["main.hero-title"] }}
-                </h4>
-                <p class="sub">
-                  {{ $store.state.translations["main.hero-sub"] }}
-                </p>
-                <div class="numbers">
-                  <button @click="openAppModal()">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="18"
-                      height="19"
-                      viewBox="0 0 18 19"
-                      fill="none"
-                    >
-                      <path
-                        d="M18 16.5V14.8541C18 14.0363 17.5021 13.3008 16.7428 12.9971L14.7086 12.1835C13.7429 11.7971 12.6422 12.2156 12.177 13.146L12 13.5C12 13.5 9.5 13 7.5 11C5.5 9 5 6.5 5 6.5L5.35402 6.32299C6.28438 5.85781 6.70285 4.75714 6.31654 3.79136L5.50289 1.75722C5.19916 0.997903 4.46374 0.5 3.64593 0.5H2C0.895431 0.5 0 1.39543 0 2.5C0 11.3366 7.16344 18.5 16 18.5C17.1046 18.5 18 17.6046 18 16.5Z"
-                        fill="#866719"
-                      />
-                    </svg>
-                    {{ $store.state.translations["main.contact-us"] }}
-                  </button>
-                  <a :href="`tel:${info.nbm}`" class="tel">
-                    {{ info?.nbm }}
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div class="swiper-slide">
-              <div class="content">
-                <p class="sup">
-                  {{ $store.state.translations["main.hero-sup"] }}
-                </p>
-                <h4 class="title">
-                  {{ $store.state.translations["main.hero-title"] }}
-                </h4>
-                <p class="sub">
-                  {{ $store.state.translations["main.hero-sub"] }}
-                </p>
-                <div class="numbers">
-                  <button @click="openAppModal()">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="18"
